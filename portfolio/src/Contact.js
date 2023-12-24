211a41d44fd44c417bbc1906f07dc8af2869d8fa
Jiya Thakur <142887456+jiyaaat@users.noreply.github.com>
2023-12-24 11:50:36 +0530

    Update Contact.js

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Contact.css';
 import EmailIcon from '@mui/icons-material/Email';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Contact = () => {
   return (
@@ -15,16 +16,24 @@ const Contact = () => {
             <div className='contact_card'>
               <EmailIcon />
               <h4>Email</h4>
-              <h5>jt2779@srmist.edu.in</h5>
-              <a href='mailto:jt2779@srmist.edu.in'>send me a message</a>
+              <h5>jiyat525@gmail.com</h5>
+              <a href='mailto:jiyat525@gmail.com'>send me a message</a>
             </div>
           </article>
           <article className='contact_option'>
             <div className='contact_card'>
-              <InstagramIcon />
-              <h4>Instagram</h4>
-              <h5>jiya_thakur17</h5>
-              <a href='https://www.instagram.com/'>send me a message</a>
+              <LinkedInIcon />
+              <h4>Linkedin</h4>
+              <h5>jiya-thakur-7ab971251</h5>
+              <a href='https://www.linkedin.com/in/jiya-thakur-7ab971251/'>send me a message</a>
+            </div>
+          </article>
+          <article className='contact_option'>
+            <div className='contact_card'>
+              <GitHubIcon />
+              <h4>Github</h4>
+              <h5>jiyaaat</h5>
+              <a href='https://github.com/jiyaaat'>send me a message</a>
             </div>
           </article>
           <article className='contact_option'>
