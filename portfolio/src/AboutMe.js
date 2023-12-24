4b1aeb13709dda0f892fd765a2f090e2accc61ab
Jiya Thakur <142887456+jiyaaat@users.noreply.github.com>
2023-12-24 11:50:07 +0530

    Update AboutMe.js

--- portfolio/src/AboutMe.js
+++ portfolio/src/AboutMe.js
@@ -9,70 +9,123 @@ function AboutMe() {
         <h1>About Me</h1>
         <div className="about-container">
           <div className="about-details">
-            <p>I'm Jiya Thakur, a second-year Computer Science Engineering student at SRM University. Currently on a journey to explore the world of web development, I'm focusing on both frontend and backend technologies. I am a passionate front-end developer focused on creating captivating web experiences. While my expertise lies in front-end, I'm also diving into backend development to build complete web applications.</p>
+            <p>I'm Jiya Thakur, a second-year Computer Science Engineering student at SRM University. Currently on a journey to explore the world of web development, I'm focusing on both frontend and backend technologies. I am a passionate front-end developer focused on creating captivating web experiences. While my expertise lies in front-end, I'm also diving into backend development to build complete web applications.Join me in this exhilarating voyage through the vast avenues of technology, where creativity meets functionality and innovation knows no bounds!</p>
           </div>
           <div className="skills-container">
             <div className="skills-card frontend">
-              <h2>Frontend</h2>
+              <h2>Development</h2>
               <ul>
                 <li>
                   <VerifiedIcon />
                   <span>HTML & CSS</span>
                   <br />
-                  <small>(intermediate)</small>
+             
                 </li>
                 <li>
                   <VerifiedIcon />
                   <span>Javascript</span>
                   <br />
-                  <small>(intermediate)</small>
+               
                 </li>
                 <li>
                   <VerifiedIcon />
                   <span>Bootstrap</span>
                   <br />
-                  <small>(intermediate)</small>
+               
                 </li>
                 <li>
                   <VerifiedIcon />
                   <span>Tailwind css</span>
                   <br />
-                  <small>(intermediate)</small>
+               
                 </li>
                 <li>
                   <VerifiedIcon />
                   <span>React</span>
                   <br />
-                  <small>(intermediate)</small>
+      
                 </li>
                 <li>
                   <VerifiedIcon />
                   <span>React-redux</span>
                   <br />
-                  <small>(intermediate)</small>
+         
                 </li>
-              </ul>
-            </div>
-            <div className="skills-card backend">
-              <h2>Backend</h2>
-              <ul>
                 <li>
                   <VerifiedIcon />
                   <span>Nodejs</span>
                   <br />
-                  <small>(basic)</small>
+              
                 </li>
                 <li>
                   <VerifiedIcon />
                   <span>Mongodb</span>
                   <br />
-                  <small>(basic)</small>
+                  
                 </li>
                 <li>
                   <VerifiedIcon />
-                  <span>Express</span>
+                  <span>Expressjs</span>
                   <br />
-                  <small>(basic)</small>
+                </li>
+                <li>
+                  <VerifiedIcon/>
+                  <span>Firebase</span>
+                  <br/>
+                </li>
+              </ul>
+            </div>
+            <div className="skills-card backend">
+              <h2>Languages</h2>
+              <ul>
+              <li>
+                  <VerifiedIcon />
+                  <span>Python</span>
+                  <br />
+              
+                </li>
+                <li>
+                  <VerifiedIcon />
+                  <span>C</span>
+                  <br />
+                  
+                </li>
+                <li>
+                  <VerifiedIcon />
+                  <span>C++</span>
+                  <br />
+                </li>
+                <li>
+                  <VerifiedIcon />
+                  <span>Java</span>
+                  <br />
+              
+                </li>
+              </ul>
+              <h2>Databases</h2>
+              <ul>
+              <li>
+                  <VerifiedIcon />
+                  <span>Mongodb</span>
+                  <br />
+                </li>
+                <li>
+                  <VerifiedIcon />
+                  <span>MySql</span>
+                  <br />
+                </li>
+              </ul>
+              <h2>Others</h2>
+              <ul>
+              <li>
+                  <VerifiedIcon />
+                  <span>Git</span>
+                  <br />
+                </li>
+                <li>
+                  <VerifiedIcon />
+                  <span>Github</span>
+                  <br />
                 </li>
               </ul>
             </div>
